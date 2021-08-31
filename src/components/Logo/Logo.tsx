7635231d974147f2a1e29d696ec20a5f362cd575
lmirzelashvili <lmirzelashvili@gmail.com>
2021-08-31 16:40:32 +0400

    Darkmodesolution

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import logoImg from "images/logo.png";
 import logoLightImg from "images/logo-light.png";
 import LogoSvg from "./LogoSvg";
 import LogoLightSvg from "./LogoLightSvg";
+import LogoDarkSvg from "./LogoDarkSvg"
+import { useAppSelector } from "app/hooks";
+import { selectDarkmodeState } from "app/darkmode/darkmode";
 
 export interface LogoProps {
   img?: string;
@@ -14,11 +17,16 @@ const Logo: React.FC<LogoProps> = ({
   img = logoImg,
   imgLight = logoLightImg,
 }) => {
+  const darkmodeState = useAppSelector(selectDarkmodeState);
+  const [isDark, setIsDark] = useState(false)
+  useEffect(() => {
+    setIsDark(darkmodeState)
+  }, [darkmodeState])
   return (
     <Link to="/" className="ttnc-logo inline-block text-primary-6000">
       {/* THIS USE FOR MY MULTI DEMO */}
       {/* IF YOU ARE MY CLIENT. PLESE DELETE THIS CODE AND YOU YOUR IMAGE PNG BY BELLOW CODE */}
-      <LogoLightSvg />
+      {isDark ? <LogoDarkSvg />: <LogoLightSvg />}
     </Link>
   );
 };
