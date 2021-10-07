ea34c216e8fecf018d370966f2e2487d359559d7
lmirzelashvili <lmirzelashvili@gmail.com>
2021-10-07 22:45:53 +0400

    NavMobile

--- src/components/Navigation/NavMobile.tsx
+++ src/components/Navigation/NavMobile.tsx
@@ -102,13 +102,12 @@ const NavMobile: React.FC<NavMobileProps> = ({
   };
 
   return (
-    <div className="overflow-y-auto w-full max-w-sm h-screen py-2 transition transform shadow-lg ring-1 dark:ring-neutral-700 bg-white dark:bg-neutral-900 divide-y-2 divide-neutral-100 dark:divide-neutral-800">
+    <div className="overflow-y-auto w-full max-w-md h-screen py-2 transition transform shadow-lg ring-1 dark:ring-neutral-700 bg-white dark:bg-neutral-900 divide-y-2 divide-neutral-100 dark:divide-neutral-800">
       <div className="py-6 px-5">
         <Logo />
         <div className="flex flex-col mt-5 text-neutral-700 dark:text-neutral-300 text-sm">
           <span>
-            Discover the most outstanding articles on all topics of life. Write
-            your stories and share them
+          ანიმუსი - არის ახალი იდეა ფსიქოთერაპიულ სივრცეში. ჩვენ ვეხმარებით ადამიანს საკუთარი რესურსების აღმოჩენის გზით, ფსიქოლოგიური გამოწვევების გადაჭრაში და თვითგანვითარებაში
           </span>
 
           <div className="flex justify-between items-center mt-4">
@@ -125,11 +124,11 @@ const NavMobile: React.FC<NavMobileProps> = ({
       <ul className="flex flex-col py-6 px-2 space-y-1">
         {data.map(_renderItem)}
       </ul>
-      <div className="flex items-center justify-between py-6 px-5 space-x-4">
+      {/* <div className="flex items-center justify-between py-6 px-5 space-x-4">
         <a href="/#" target="_blank" rel="noopener noreferrer">
           <ButtonPrimary>Get Template</ButtonPrimary>
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
