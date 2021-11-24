951f180df5c03602b97dcf13291763f83810dbcb
georgeadeishvili <giorgi.adeishvili@gmail.com>
2021-11-24 08:30:35 +0400

    done

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,18 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    // baseURL: 'https://animus.ge:4000/',
-    baseURL: 'http://localhost:4000/'
+	baseURL: 'https://animus.ge:4000/'
+	// baseURL: 'http://localhost:4000/'
 })
 
-axios.interceptors.request.use((req) => {
-    req.headers['Authorization'] = `Bearer ${localStorage.getItem('customer-token')}`
-    return req
-}, (e) => {
-    return Promise.reject(e)
-})
+axios.interceptors.request.use(
+	req => {
+		req.headers['Authorization'] = `Bearer ${localStorage.getItem('customer-token')}`
+		return req
+	},
+	e => {
+		return Promise.reject(e)
+	}
+)
 
 export default axios
